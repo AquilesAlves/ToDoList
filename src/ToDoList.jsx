c81a31cb262103687f2d688824d77c660d9c8db2
Aquiles Alves <aquilespereira789@gmail.com>
2026-02-26 19:26:25 -0300

    ab

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,27 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 import './ToDoList.css';
+import Icone from './assets/icone.webp'
 
 function ToDoList() {
+
+    const [lista, setLista] = useState([])
+    const [novoItem, setNovoItem] = useState('')
+
+    function adicionaItem(form) {
+        form.preventDefault();
+        if (!novoItem) {
+            return
+        }
+        setLista([...lista, {text: novoItem, isCompleted: false}])
+        setNovoItem('')
+        document.getElementById('input-entrada').focus()
+
+    }
+
+    function clicou(index) {
+        const listaAux = [...lista]
+        listaAux[index].isCompleted = !listaAux[index].isCompleted
+        setLista(listaAux)
+    }
+
     return (
         <div>
             <h1>Lista de Tarefas</h1>
-            <form>
+            <form onSubmit={adicionaItem} >
                 <input 
-                    type="text" placeholder="Adicione uma Tarefa"
+                    id="input-entrada"
+                    type="text"
+                    value={novoItem} 
+                    onChange={(e) => {setNovoItem(e.target.value)} }
+                    placeholder="Adicione uma Tarefa"
                 />
                 <button className="add" type="submit">
                     Add
                 </button>
             </form>
             <div className="listaTarefas">
-                <div className="item">
-                    <span>Tarefa de exemplo</span>
-                    <button>Deletar</button>
+                <div style={{textAlign: 'center'}}>
+                    {
+                        lista.length <1
+                            ?
+                            <img className="icone-central" src={Icone}/>
+                            :
+                            lista.map((item, index) => (
+                                <div 
+                                    key={index}
+                                    className={item.isCompleted ? 'item completo' : 'item'}
+                                >
+                                    <span onClick={() => {clicou(index)} } >{item.text}</span>
+                                    <button className="del">Deletar</button>
+                                </div>
+                            ))
+ 
+                    }
                 </div>
-                <div className="item completo">
-                    <span>Tarefa de exemplo</span>
-                    <button>Deletar</button>
-                </div>
+
+                <button className="delAll">Deletar Todos</button>
+            
             </div>
 
 
