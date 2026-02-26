cbda8e981f5e8dfe4fdd968453a72ba54708cf21
Aquiles Alves <aquilespereira789@gmail.com>
2026-02-26 17:28:11 -0300

    u

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import './ToDoList.css';
 
 function ToDoList() {
     return (
-        <h1>Olá mundo!</h1>
+        <div>
+            <h1>Lista de Tarefas</h1>
+            <form>
+                <input 
+                    type="text" placeholder="Adicione uma Tarefa"
+                />
+                <button type="submit">
+                    Add
+                </button>
+            </form>
+            <div className="listaTarefas">
+                <div className="item">
+                    <span>Tarefa de exemplo</span>
+                    <button>Deletar</button>
+                </div>
+                <div className="item completo">
+                    <span>Tarefa de exemplo</span>
+                    <button>Deletar</button>
+                </div>
+            </div>
+
+
+        </div>
     )
 }
 
